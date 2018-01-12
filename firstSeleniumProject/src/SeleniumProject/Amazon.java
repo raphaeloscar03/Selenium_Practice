af0e019bf00d2481aa1d0bda2d6ac6d23ff3ccf0
Tools lab4 <Tools lab4@TMZ-DPC-2033-PC>
2018-01-12 11:09:36 +0530

    This is a Test Line

--- firstSeleniumProject/src/SeleniumProject/Amazon.java
+++ firstSeleniumProject/src/SeleniumProject/Amazon.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Amazon {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+		// This is a Test Line
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
 		WebDriver cdriver = new ChromeDriver();
