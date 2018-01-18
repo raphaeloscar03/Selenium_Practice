5651496d016f7bdd94f266bc42c8c02faf054784
Tools lab4 <Tools lab4@TMZ-DPC-2033-PC>
2018-01-18 15:22:40 +0530

    TestNG Changes

--- firstSeleniumProject/src/SeleniumProject/Calculator.java
+++ firstSeleniumProject/src/SeleniumProject/Calculator.java
@@ -1,51 +1,61 @@
 package SeleniumProject;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
+import org.testng.annotations.Test;
+
+@Test
 public class Calculator {
 	
 	float c;
 	
+	@Test
 	public float add(float a, float b) {
 		
 		c = a + b;
 		return c;
 	}
 	
+	@Test
 	public float sub(float a, float b) {
 		
 		c = a - b;
 		return c;
 	}
 	
+	@Test
 	public float mul(float a, float b) {
 		
 		c = a * b;
 		return c;
 	}
 	
+	@Test
 	public float div(float a, float b) {
 		
 		c = a/b;
 		return c;
 	}
 
-	
+	@Test
 	public static void main(String[] args) {
 		
 		Calculator cal = new Calculator();
-		Scanner Scan;
-		Scan = new Scanner(System.in);
-		System.out.println("Please enter the addition variables");
-		float Val1 = Scan.nextFloat();
-		float Val2 = Scan.nextFloat();
-		System.out.println("Please enter the action......\n"
-				+ "For Add Press '1'\n"
-				+ "For Sub Press '2'\n"
-				+ "For Mul Press '3'\n"
-				+ "For Div Press '4'\n");
-		int calc = Scan.nextInt(); 
-		Scan.close();
+		//Scanner Scan;
+		//Scan = new Scanner(System.in);
+		//System.out.println("Please enter the addition variables");
+		//float Val1 = Scan.nextFloat();
+		//float Val2 = Scan.nextFloat();
+		//System.out.println("Please enter the action......\n"
+		//	+ "For Add Press '1'\n"
+		//	+ "For Sub Press '2'\n"
+		//	+ "For Mul Press '3'\n"
+		//	+ "For Div Press '4'\n");
+		//int calc = Scan.nextInt(); 
+		//Scan.close();
+		float Val1 = 25;
+		float Val2 = 32;
+		int calc = 3;
 		
 		switch(calc)
 		{
